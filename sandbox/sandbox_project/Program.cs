41ed9536721ccadf8183d159117f324ce35a43ff
michael6520 <mil24044@byui.edu>
2025-01-12 22:41:27 -0700

    Week 01

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -4,10 +4,32 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
+        List<int> numbers = [1, 2, 3, 4, 5, 6];
+        RotateListRight(numbers, 2);
+    }
 
-        Console.WriteLine("Hello Sandbox World!");
+    static void RotateListRight(List<int> data, int amount)
+    {
+        List<int> lastItems = new();
+        for (int i = data.Count - 1, counter = 0; counter < amount; i--, counter++)
+        {
+            lastItems.Add(data[i]);
+        }
+
+        int index;
+        for (index = data.Count - 1; (index - amount) >= 0; index--)
+        {
+            data[index] = data[index - amount];
+        }
+
+        for (int i = 0; i < lastItems.Count; i++, index--)
+        {
+            data[index] = lastItems[i];
+        }
+
+        foreach (var item in data)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,33 +1,30 @@
 public static class Arrays
 {
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create an empty array whose length is the length variable
+        // Use a for loop to create a variable, i, that starts at zero and increments by one each loop until it's as big as the length variable
+        // In each loop, change the value of the array at i to number times (i plus 1), then return the array
 
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++) 
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples;
     }
 
-    /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
-    /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Save the last 'amount' items in 'data' to a new list
+        // Remove the last 'amount' items from 'data'
+        // Add the contents of the list you made to the very front of 'data'
+
+        List<int> overflow = data.GetRange(data.Count - amount, amount);
+        data.RemoveRange(data.Count - amount, amount);
+        data.InsertRange(0, overflow);
+
+        Console.WriteLine(data);
     }
 }
