c172e696b343ee88dd3c90725053ba89e4889a60
michael6520 <mil24044@byui.edu>
2025-02-08 17:36:52 -0700

    Week 05

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -14,8 +14,9 @@ public static class Recursion
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -39,7 +40,15 @@ public static class Recursion
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        for (int i = 0; i < letters.Length; i++)
+        {
+            PermutationsChoose(results, letters.Substring(0, i) + letters.Substring(i + 1), size, word + letters[i]);
+        }
     }
 
     /// <summary>
@@ -86,7 +95,8 @@ public static class Recursion
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
-        // Base Cases
+        remember ??= [];
+
         if (s == 0)
             return 0;
         if (s == 1)
@@ -96,10 +106,13 @@ public static class Recursion
         if (s == 3)
             return 4;
 
-        // TODO Start Problem 3
+        if (remember.ContainsKey(s))
+        {
+            return remember[s];
+        }
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        remember[s] = ways;
         return ways;
     }
 
@@ -118,7 +131,22 @@ public static class Recursion
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        if (!pattern.Contains('*'))
+        {
+            results.Add(pattern);
+            return;
+        }
+        
+        char[] chars = pattern.ToCharArray();
+
+        int index = Array.IndexOf(chars, '*');
+        
+        for (int i = 0; i < 2; i++)
+        {
+            chars[index] = (char)(i + 48);
+            pattern = new string(chars);
+            WildcardBinary(pattern, results);
+        }    
     }
 
     /// <summary>
