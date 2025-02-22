ca718e5d7cec223489a58f413298c69c2c3ca902
michael6520 <mil24044@byui.edu>
2025-02-22 10:42:02 -0700

    Week 06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,7 +11,10 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data)
+        {
+            return;
+        }
 
         if (value < Data)
         {
@@ -33,13 +36,64 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+        else
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int left = 0;
+        int right = 0;
+        
+        if (Right is null)
+        {
+            if (Left is null)
+            {
+                return 1;
+            }
+            else
+            {
+                left = Left.GetHeight();
+            }
+        }
+        else
+        {
+            if (Left is null)
+            {
+                right = Right.GetHeight();
+            }
+            else
+            {
+                left = Left.GetHeight();
+                right = Right.GetHeight();
+            }
+        }
+
+        return 1 + Math.Max(left, right);
     }
 }
